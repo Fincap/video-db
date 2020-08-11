dfb4e2997f54d89502b79622435ed4802be2510b
fincap <finnkerman@gmail.com>
2020-08-11 22:56:26 +1000

    Add .gitignore

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sqlite3
 from db import DatabaseController
 
 if __name__ == '__main__':
-    conn = sqlite3.connect("tables.db")
+    conn = sqlite3.connect("data/tables.db")
     database_controller = DatabaseController(conn)
     print(database_controller.get_table_names())
 
