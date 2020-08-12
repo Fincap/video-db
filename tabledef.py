e3cb1cc9e93600817b8a8ddcdd82b2f791331ca3
fincap <finnkerman@gmail.com>
2020-08-12 15:02:45 +1000

    Fixed syntax error.

--- tabledef.py
+++ tabledef.py
@@ -9,7 +9,7 @@ title text NOT NULL)
 # Tags table definition
 TABLE_TAGS = """
 CREATE TABLE tags (
-tag_id integer PRIMARY KEY
+tag_id integer PRIMARY KEY,
 video_id integer NOT NULL,
 tag_text text NOT NULL,
 FOREIGN KEY (video_id)
