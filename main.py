2edc00a7e648a518e8aafdbc5f0b09ed7f9ad979
fincap <finnkerman@gmail.com>
2020-08-12 15:10:29 +1000

    Created function to generate objects of Videos from db.

--- main.py
+++ main.py
@@ -2,12 +2,31 @@ import logging
 import sqlite3
 
 from db import DatabaseController
+from objects import get_videos_list
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
 
-    conn = sqlite3.connect("data/tables.db")
+    conn = sqlite3.connect(":memory:")
     database_controller = DatabaseController(conn)
-    print(database_controller.get_table_names())
+    print("Tables:", database_controller.get_table_names())
+
+    database_controller.add_new_video(1, "youtube.com/hj34Hd8", "Funny cat video!!")
+    database_controller.add_tag(1, "animal")
+    database_controller.add_tag(1, "comedy")
+    database_controller.add_tag(1, "cat")
+
+    database_controller.add_new_video(2, "youtube.com/h4Hd8jk", "Funny dog video :0")
+    database_controller.add_tag(2, "animal")
+    database_controller.add_tag(2, "comedy")
+    database_controller.add_tag(2, "dog")
+
+    database_controller.add_new_video(3, "youtube.com/ky9rjDF", "sad turtle video :(")
+    database_controller.add_tag(3, "animal")
+    database_controller.add_tag(3, "drama")
+    database_controller.add_tag(3, "turtle")
+
+    for video in get_videos_list(database_controller):
+        print(video)
 
     conn.close()
--- objects.py
+++ objects.py
@@ -1,6 +1,29 @@
+from db import DatabaseController
+
+
+def get_videos_list(controller: DatabaseController) -> list:
+    videos = []
+
+    video_tuples = controller.get_videos()
+    for video_tuple in video_tuples:
+        new_video_id = video_tuple[0]
+        new_video_url = video_tuple[1]
+        new_video_title = video_tuple[2]
+
+        tag_tuples = controller.get_tags(video_tuple[0])
+        new_tags = []
+        for tag_tuple in tag_tuples:
+            new_tags.append(tag_tuple[2])
+
+        new_video = Video(new_video_id, new_video_url, new_video_title, new_tags)
+        videos.append(new_video)
+
+    return videos
+
+
 class Video:
 
-    def __init__(self, video_id: int, url: str, title: str, tags: list) -> None:
+    def __init__(self, video_id: int, url: str, title: str, tags:list) -> None:
         self.video_id = video_id
         self.url = url
         self.title = title
