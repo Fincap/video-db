8f9e965f34cd21ceb9d05aedcb947baf48ecfbdd
fincap <finnkerman@gmail.com>
2020-08-12 15:52:37 +1000

    Expand functionality of Video object generation functions.

--- objects.py
+++ objects.py
@@ -1,26 +1,6 @@
 from db import DatabaseController
 
 
-def get_videos_list(controller: DatabaseController) -> list:
-    videos = []
-
-    video_tuples = controller.get_videos()
-    for video_tuple in video_tuples:
-        new_video_id = video_tuple[0]
-        new_video_url = video_tuple[1]
-        new_video_title = video_tuple[2]
-
-        tag_tuples = controller.get_tags(video_tuple[0])
-        new_tags = []
-        for tag_tuple in tag_tuples:
-            new_tags.append(tag_tuple[2])
-
-        new_video = Video(new_video_id, new_video_url, new_video_title, new_tags)
-        videos.append(new_video)
-
-    return videos
-
-
 class Video:
 
     def __init__(self, video_id: int, url: str, title: str, tags:list) -> None:
@@ -31,3 +11,30 @@ class Video:
 
     def __str__(self) -> str:
         return "%s: %s (%s), %s" % (self.video_id, self.title, self.url, self.tags)
+
+
+def generate_videos_list(controller: DatabaseController) -> list:
+    videos = []
+
+    video_tuples = controller.get_videos()
+    for video_tuple in video_tuples:
+        new_video_id = video_tuple[0]
+
+        videos.append(generate_video(controller, new_video_id))
+
+    return videos
+
+
+def generate_video(controller: DatabaseController, video_id: int) -> Video:
+    video_tuple = controller.get_video_by_id(video_id)
+
+    new_video_id = video_tuple[0]
+    new_video_url = video_tuple[1]
+    new_video_title = video_tuple[2]
+
+    tag_tuples = controller.get_tags(video_id)
+    new_tags = []
+    for tag_tuple in tag_tuples:
+        new_tags.append(tag_tuple[2])
+
+    return Video(new_video_id, new_video_url, new_video_title, new_tags)
