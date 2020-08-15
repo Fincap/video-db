5ec1cf4985e41b44414c28cd508809ff4b872b49
fincap <finnkerman@gmail.com>
2020-08-15 16:42:35 +1000

    Corrected issue with tags table definition.

--- tabledef.py
+++ tabledef.py
@@ -3,7 +3,7 @@ TABLE_VIDEOS = """
 CREATE TABLE videos (
 video_id integer PRIMARY KEY,
 url text NOT NULL,
-title text NOT NULL)
+title text NOT NULL);
 """
 
 # Tags table definition
@@ -13,5 +13,6 @@ tag_id integer PRIMARY KEY,
 video_id integer NOT NULL,
 tag_text text NOT NULL,
 FOREIGN KEY (video_id)
-    REFERENCES videos (video_id) )
+    REFERENCES videos (video_id)
+    ON DELETE CASCADE );
 """
